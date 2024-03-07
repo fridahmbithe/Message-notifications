ce166cd0edc0dc9d0737dfb617d05008e6b87ef4
fridahmbithe <fridahmbithe45@gmail.com>
2024-03-07 20:04:14 +0300

    updated code

--- src/Components/ChatWindow.js
+++ src/Components/ChatWindow.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import "./ChatWindow.css";
+import { upload } from "@testing-library/user-event/dist/upload";
 
 export const ChatWindow = () => {
   const [messages, setMessages] = useState([]);
@@ -21,8 +22,16 @@ export const ChatWindow = () => {
   };
 
   const handleFileChange = (event) => {
-    setFile(event.target.files[0]);
+    const selectedFile = event.target.files[0];
+    setFile(selectedFile);
+
+    if (selectedFile) {
+      console.log("Selected file:", selectedFile);
+    } else {
+      console.log("No file selected");
+    }
   };
+
   useEffect(() => {
     // Parse the message from the query parameter
     const query = new URLSearchParams(window.location.search);
@@ -48,25 +57,22 @@ export const ChatWindow = () => {
                 {message.isSentByUser ? (
                   <>
                     <span className="sent-by-user">{message.text}</span>
-                    {/* {message.content && (
+                    {message.content && (
                       <div>
                         <label htmlFor={`file-input-${index}`}> Attach </label>
                         <input
                           type="file"
                           id={`file-input-${index}`}
-                          // style={{
-                          //   fontSize: "1rem",
-                          //   padding: "10px",
-                          //   borderRadius: "3px",
-                          // }}
-                          style={{display:'inherit'}}
-
+                          style={{
+                            display: "none",
+                          }}
                           onChange={(e) => handleFileChange(e, index)}
                           accept="image/*, .pdf, .doc, .docx, .xls, .xlsx"
                         />
-                        Attachment: {message.file ? message.file.name : "attach"}
+                        Attachment:{" "}
+                        {message.file ? message.file.name : "attach"}
                       </div>
-                    )} */}
+                    )}
                   </>
                 ) : (
                   <>
@@ -89,13 +95,27 @@ export const ChatWindow = () => {
             onChange={(e) => setMessageText(e.target.value)}
             placeholder="Enter your message..."
           />
-          <input
-            type="file"
-            id="file-input"
-            onChange={handleFileChange}
-            accept="image/*, .pdf, .doc, .docx, .xls, .xlsx"
-            style={{ display: "none" }}
-          />
+          <label htmlFor="file-input">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="w-5 h-5"
+            >
+              <path d="M12.232 4.232a2.5 2.5 0 0 1 3.536 3.536l-1.225 1.224a.75.75 0 0 0 1.061 1.06l1.224-1.224a4 4 0 0 0-5.656-5.656l-3 3a4 4 0 0 0 .225 5.865.75.75 0 0 0 .977-1.138 2.5 2.5 0 0 1-.142-3.667l3-3Z" />
+              <path d="M11.603 7.963a.75.75 0 0 0-.977 1.138 2.5 2.5 0 0 1 .142 3.667l-3 3a2.5 2.5 0 0 1-3.536-3.536l1.225-1.224a.75.75 0 0 0-1.061-1.06l-1.224 1.224a4 4 0 1 0 5.656 5.656l3-3a4 4 0 0 0-.225-5.865Z" />
+            </svg>
+            <input
+              type="file"
+              id="file-input"
+              onChange={(e) => handleFileChange(e)}
+              accept="image/*, .pdf, .doc, .docx, .xls, .xlsx"
+              style={{ display: "none" }}
+            />
+            Upload {""}
+            {upload.file ? file.name : ""}
+          </label>
+
           <button type="submit" className="msger-send-btn">
             Send
           </button>
